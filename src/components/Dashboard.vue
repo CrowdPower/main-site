beb9e456e2b33f50a570f2d04604c87dd166a4b6
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-02-05 11:16:10 -0500

    Redirect to and from login based on authentication using the router

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -10,10 +10,6 @@ export default {
   created: () => {
     this.refreshToken = Cookies.get('refreshToken')
     this.accessToken = Cookies.get('accessToken')
-    if (!(this.refreshToken && this.accessToken)) {
-      this.$router.push('/login')
-    }
-    console.log(this.accessToken)
   },
   methods: {
   }
--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,11 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Login from '@/components/Login'
 import Dashboard from '@/components/Dashboard'
+import Cookies from 'js-cookie'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
@@ -25,3 +26,27 @@ export default new Router({
     }
   ]
 })
+
+function isAuthenticated () {
+  return Cookies.get('refreshToken') && Cookies.get('accessToken')
+}
+
+let authProtected = ['dashboard']
+let redirectToDashboard = ['', 'login']
+
+router.beforeEach((to, from, next) => {
+  let path = to.path.split('/')[1]
+  if (isAuthenticated()) {
+    if (redirectToDashboard.includes(path)) {
+      next({ path: '/dashboard' })
+    } else {
+      next()
+    }
+  } else if (authProtected.includes(path)) {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
+
+export default router
